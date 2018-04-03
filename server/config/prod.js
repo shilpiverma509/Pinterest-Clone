1a9404a56d3aab270b4e7760548bd2c0b0f1fd78
Shilpi Verma <shilpiverma509@gmail.com>
2018-04-02 20:26:41 -0500

    modified prodKeys

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
-  googleClientSecret: process.env.TWITTER_CONSUMER_SECRET,
+  TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET,
   mongooseURI: process.env.MONGOOSEURI,
   cookieKey: process.env.COOKIE_KEY
 };
