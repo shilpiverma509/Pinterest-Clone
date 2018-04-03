1c6b58595d24a5219eaf472d157d27e2d719a355
Shilpi Verma <shilpiverma509@gmail.com>
2018-04-02 20:21:20 -0500

    added production keys

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
+  googleClientSecret: process.env.TWITTER_CONSUMER_SECRET,
+  mongooseURI: process.env.MONGOOSEURI,
+  cookieKey: process.env.COOKIE_KEY
+};
